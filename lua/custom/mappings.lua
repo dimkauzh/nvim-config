90fa1f492a3e19b78bec13536a3ca4bc226edfa5
Dima Marhitych <uzhdimka@gmail.com>
2024-03-10 10:18:21 +0100

    Some updates to fix Debian+Gopls+Gpaste

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,6 +7,22 @@ M.general = {
   },
 }
 
+-- Gpaste
+if vim.fn.executable('gpaste-client') == 1 then
+    vim.g.clipboard = {
+        name = 'gpaste',
+        copy = {
+                ["+"] = { 'gpaste-client' },
+                ["*"] = { 'gpaste-client' },
+        },
+        paste = {
+                ["+"] = { 'gpaste-client', '--use-index', 'get', '0' },
+                ["*"] = { 'gpaste-client', '--use-index', 'get', '0' },
+        },
+        cache_enabled = true,
+    }
+end
+
 -- Save
 
 vim.keymap.set('n', '<D-s>', ':w<CR>')
