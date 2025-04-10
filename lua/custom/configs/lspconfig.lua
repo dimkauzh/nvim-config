b80f24cdf3d72d85daf8bd99b80256d489a7cab9
Dima <uzhdimka@gmail.com>
2025-04-11 00:23:11 +0200

    added ols for odin

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "clangd", "gopls", "pyright", "nixd" }
+local servers = { "clangd", "gopls", "pyright", "nixd", "ols" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
